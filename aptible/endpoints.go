1b3e513046620003daa75b5c0d50a11107167e28
reggregory <rachel.gregory@aptible.com>
2020-02-27 23:20:55 -0800

    Added structs for creating and updating, added helper funcs for string slice -> interface slice, getting endpoint types

--- aptible/endpoints.go
+++ aptible/endpoints.go
@@ -8,22 +8,36 @@ import (
 	"github.com/reggregory/go-deploy/models"
 )
 
+type Updates struct {
+	ContainerPort int64
+	IPWhitelist   []string
+	Platform      string
+}
+
+type CreateAttrs struct {
+	Type          *string
+	Default       bool
+	Internal      bool
+	ContainerPort int64
+	IPWhitelist   []string
+	Platform      string
+}
+
 // CreateEndpoint() creates Vhost API object + provision operation on the app.
-func (c *Client) CreateEndpoint(app_id int64, attrs map[string]interface{}) (*models.InlineResponse2019, error) {
+func (c *Client) CreateEndpoint(app_id int64, attrs CreateAttrs) (*models.InlineResponse2019, error) {
 	service_id, err := c.GetServiceID(app_id)
 	if err != nil {
 		return nil, err
 	}
 
 	// Create Vhost API object
-	req_type := "http_proxy_protocol"
 	app_req := models.AppRequest33{
-		Type:          &req_type,
-		Default:       true,
-		Internal:      attrs["internal"].(bool),
-		ContainerPort: attrs["container_port"].(int64),
-		IPWhitelist:   attrs["ip_filtering"].([]string),
-		Platform:      attrs["platform"].(string),
+		Type:          attrs.Type,
+		Default:       attrs.Default,
+		Internal:      attrs.Internal,
+		ContainerPort: attrs.ContainerPort,
+		IPWhitelist:   attrs.IPWhitelist,
+		Platform:      attrs.Platform,
 	}
 
 	params := operations.NewPostServicesServiceIDVhostsParams().WithServiceID(service_id).WithAppRequest(&app_req)
@@ -32,11 +46,10 @@ func (c *Client) CreateEndpoint(app_id int64, attrs map[string]interface{}) (*mo
 		return nil, err
 	}
 
+	// Create "provision" operation
 	payload := resp.Payload
 	endpoint_id := *payload.ID
-
-	// Create "provision" operation
-	req_type = "provision"
+	req_type := "provision"
 	op_req := models.AppRequest26{Type: &req_type}
 	op_params := operations.NewPostVhostsVhostIDOperationsParams().WithVhostID(endpoint_id).WithAppRequest(&op_req)
 	_, err = c.Client.Operations.PostVhostsVhostIDOperations(op_params, c.Token)
@@ -44,7 +57,7 @@ func (c *Client) CreateEndpoint(app_id int64, attrs map[string]interface{}) (*mo
 		return nil, err
 	}
 
-	// Wait for endpoint to be provisioned.
+	// Wait for endpoint to be provisioned
 	for *payload.Status != "provisioned" {
 		time.Sleep(1 * time.Second)
 		payload, _, err = c.GetEndpoint(endpoint_id)
@@ -76,19 +89,11 @@ func (c *Client) GetEndpoint(endpoint_id int64) (*models.InlineResponse2019, boo
 }
 
 // UpdateEndpoint() takes in an endpoint_id and updates needed, and updates the endpoint.
-func (c *Client) UpdateEndpoint(endpoint_id int64, updates map[string]interface{}) error {
-	app_req := models.AppRequest34{}
-
-	if _, ok := updates["container_port"]; ok {
-		app_req.ContainerPort = updates["container_port"].(int64)
-	}
-
-	if _, ok := updates["ip_filtering"]; ok {
-		app_req.IPWhitelist = updates["ip_filtering"].([]string)
-	}
-
-	if _, ok := updates["platform"]; ok {
-		app_req.Platform = updates["platform"].(string)
+func (c *Client) UpdateEndpoint(endpoint_id int64, up Updates) error {
+	app_req := models.AppRequest34{
+		ContainerPort: up.ContainerPort,
+		IPWhitelist:   up.IPWhitelist,
+		Platform:      up.Platform,
 	}
 
 	params := operations.NewPutVhostsIDParams().WithID(endpoint_id).WithAppRequest(&app_req)
@@ -124,7 +129,24 @@ func (c *Client) GetServiceID(app_id int64) (int64, error) {
 	// for i := 0; i < len(services); i++ {
 	// TODO: add logic for finding "right" service
 	// }
-
 	id := services[0].ID
 	return id, err
+}
+
+func GetEndpointType(t string) (string, error) {
+	switch t {
+	case "HTTPS":
+		return "http_proxy_protocol", nil
+	default:
+		e := fmt.Errorf("Invalid endpoint type. The only valid types are HTTPS, TLS, and TCP.")
+		return "", e
+	}
+}
+
+func MakeStringSlice(if_slice []interface{}) ([]string, error) {
+	str_slice := make([]string, len(if_slice))
+	for i := 0; i < len(if_slice); i++ {
+		str_slice[i] = (if_slice[i].(string))
+	}
+	return str_slice, nil
 }
