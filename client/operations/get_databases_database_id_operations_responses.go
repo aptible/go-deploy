a550ebed3ccd06da6cac8c747eb3f812d8e035bf
reggregory <rachel.gregory@aptible.com>
2020-03-23 13:19:57 -0500

    aptible -> reggregory for local dev

--- client/operations/get_databases_database_id_operations_responses.go
+++ client/operations/get_databases_database_id_operations_responses.go
@@ -13,7 +13,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/aptible/go-deploy/models"
+	models "github.com/reggregory/go-deploy/models"
 )
 
 // GetDatabasesDatabaseIDOperationsReader is a Reader for the GetDatabasesDatabaseIDOperations structure.
