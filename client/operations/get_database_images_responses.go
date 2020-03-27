de4ca7ed254e36bd8db11b75a3a67d770e4b1071
reggregory <rachel.gregory@aptible.com>
2020-03-27 08:40:47 -0500

    reggregory -> aptible for PR

--- client/operations/get_database_images_responses.go
+++ client/operations/get_database_images_responses.go
@@ -13,7 +13,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/reggregory/go-deploy/models"
+	models "github.com/aptible/go-deploy/models"
 )
 
 // GetDatabaseImagesReader is a Reader for the GetDatabaseImages structure.
