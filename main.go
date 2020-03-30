2702bb41443ed92eb029f989ff044b67a277d5c4
reggregory <rachel.gregory@aptible.com>
2020-03-30 18:43:17 -0500

    revert accidental changes to main.go, go.mod

--- main.go
+++ main.go
@@ -19,53 +19,6 @@ func main() {
 		fmt.Println(op)
 	}
 
-	c, err := aptible.SetUpClient()
-	if err != nil {
-		log.Fatalf("couldn't do it: %s", err)
-	}
-
-	prov_op, err := c.GetLatestProvisionOperation(33)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Op ID: ", prov_op.ID)
-		fmt.Println("Container size: ", *prov_op.ContainerSize)
-		fmt.Println("Disk size: ", prov_op.DiskSize)
-	}
-
-	// attrs := aptible.ReplicateAttrs{
-	// 	EnvID:         4,
-	// 	DatabaseID:    25,
-	// 	ReplicaHandle: "replica-wait",
-	// }
-
-	// // tests getting database from handle
-	// database, err := c.GetDatabaseFromHandle(4, "repli")
-	// if err != nil {
-	// 	fmt.Println("couldn't do it: ", err)
-	// } else {
-	// 	fmt.Println("database handle + ID:", database.Handle, database.ID)
-	// }
-
-	// // tests creating replica
-	// replica, err := c.CreateReplica(attrs)
-	// if replica != nil {
-	// 	id := replica.ID
-
-	// 	time.Sleep(20 * time.Second)
-
-	// 	err = deleteReplica(id)
-	// 	if err != nil {
-	// 		log.Fatalf("couldn't do it: %s", err)
-	// 	} else {
-	// 		fmt.Println("Successfully deleted replica!")
-	// 	}
-
-	// } else {
-	// 	fmt.Println("Replication failed.")
-	// }
-}
-
 func getOperations() ([]string, error) {
 	c, err := aptible.SetUpClient()
 	if err != nil {
@@ -85,13 +38,4 @@ func getOperations() ([]string, error) {
 		lines = append(lines, line)
 	}
 	return lines, nil
-}
-
-func deleteReplica(replica_id int64) error {
-	c, err := aptible.SetUpClient()
-	if err != nil {
-		return err
-	}
-
-	return c.DeleteReplica(replica_id)
 }
