247bf2a958774b4f5f2fe5af37510a5f8177c050
reggregory <rachel.gregory@aptible.com>
2020-04-13 11:17:09 -0500

    reggregory -> aptible for PR

--- client/operations/get_vhosts_vhost_id_operations_responses.go
+++ client/operations/get_vhosts_vhost_id_operations_responses.go
@@ -13,7 +13,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/reggregory/go-deploy/models"
+	models "github.com/aptible/go-deploy/models"
 )
 
 // GetVhostsVhostIDOperationsReader is a Reader for the GetVhostsVhostIDOperations structure.
