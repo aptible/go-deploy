3a3aa294d28b0e217179337c038217af9ba2c550
reggregory <rachel.gregory@aptible.com>
2020-04-20 10:39:19 -0500

    aptible -> reggregory for local dev

--- aptible/database.go
+++ aptible/database.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aptible/go-deploy/client/operations"
-	"github.com/aptible/go-deploy/models"
+	"github.com/reggregory/go-deploy/client/operations"
+	"github.com/reggregory/go-deploy/models"
 )
 
 type DBUpdates struct {
--- client/operations/patch_operations_id_parameters.go
+++ client/operations/patch_operations_id_parameters.go
@@ -17,7 +17,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/aptible/go-deploy/models"
+	models "github.com/reggregory/go-deploy/models"
 )
 
 // NewPatchOperationsIDParams creates a new PatchOperationsIDParams object
