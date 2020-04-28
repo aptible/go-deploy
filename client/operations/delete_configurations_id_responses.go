5db0ffaea3ef9ac81879220cac4a0cd4013b8836
reggregory <rachel.gregory@aptible.com>
2020-04-28 15:44:19 -0500

    aptible -> reggregory for local dev (again)

--- client/operations/delete_configurations_id_responses.go
+++ client/operations/delete_configurations_id_responses.go
@@ -13,7 +13,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	models "github.com/aptible/go-deploy/models"
+	models "github.com/reggregory/go-deploy/models"
 )
 
 // DeleteConfigurationsIDReader is a Reader for the DeleteConfigurationsID structure.
