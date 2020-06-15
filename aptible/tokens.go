de8cda1a68d2d39270c5ab250f53144c0c52145e
Robert Fairhead <robert.fairhead@aptible.com>
2020-06-15 11:02:16 -0500

    Remove printf debugging statements

--- aptible/tokens.go
+++ aptible/tokens.go
@@ -18,7 +18,6 @@ func GetToken() (string, error) {
 	}
 
 	home, err := homedir.Dir()
-	fmt.Println(home)
 	if err != nil {
 		return "", err
 	}
@@ -39,9 +38,6 @@ func GetToken() (string, error) {
 	if auth == "" {
 		auth = "https://auth.aptible.com"
 	}
-
-	fmt.Println(auth)
-	fmt.Printf("%+v\n", tokens)
 
 	// Checks if there is a token.
 	token = tokens[auth]
