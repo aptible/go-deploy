13b6eb5825ac63f2ad49f9077d5168ce9fa40b80
Eric Bower <me@erock.io>
2021-07-08 10:49:14 -0400

    add operations

--- aptible/log_drain.go
+++ aptible/log_drain.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aptible/go-deploy/client/operations"
 	"github.com/aptible/go-deploy/models"
 	strfmt "github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
 )
 
 type LogDrain struct {
@@ -64,19 +65,26 @@ func (c *Client) CreateLogDrain(handle string, accountID int64, attrs *LogDrainC
 		return logDrain, fmt.Errorf("log drain ID is a nil pointer")
 	}
 	logDrain.ID = *response.Payload.ID
-	logDrain.Handle = *response.Payload.Handle
-	logDrain.DrainType = *response.Payload.DrainType
-	logDrain.DrainHost = *response.Payload.DrainHost
-	logDrain.DrainPort = *response.Payload.DrainPort
-	logDrain.LoggingToken = *response.Payload.LoggingToken
-	logDrain.URL = *response.Payload.URL
-	logDrain.DrainApps = *response.Payload.DrainApps
-	logDrain.DrainDatabases = *response.Payload.DrainDatabases
-	logDrain.DrainEphemeralSessions = *response.Payload.DrainEphemeralSessions
-	logDrain.DrainProxies = *response.Payload.DrainProxies
-	logDrain.Deleted = false
 
-	return logDrain, nil
+	requestType := "provision"
+	operationRequest := &models.AppRequest29{Type: &requestType}
+	operationParams := operations.NewPostLogDrainsLogDrainIDOperationsParams().WithLogDrainID(logDrain.ID).WithAppRequest(operationRequest)
+	operationResponse, err := c.Client.Operations.PostLogDrainsLogDrainIDOperations(operationParams, c.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	// Wait on provision operation to complete.
+	if operationResponse.Payload.ID == nil {
+		return nil, fmt.Errorf("operation ID is a nil pointer")
+	}
+	operationID := *operationResponse.Payload.ID
+	_, err = c.WaitForOperation(operationID)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.GetLogDrain(logDrain.ID)
 }
 
 func (c *Client) GetLogDrain(logDrainID int64) (*LogDrain, error) {
@@ -90,7 +98,7 @@ func (c *Client) GetLogDrain(logDrainID int64) (*LogDrain, error) {
 		errStruct := err.(*operations.GetLogDrainsIDDefault)
 		switch errStruct.Code() {
 		case 404:
-			// If deleted == true, then the endpoint needs to be removed from Terraform.
+			// If deleted == true, then the log_drain needs to be removed from Terraform.
 			logDrain.Deleted = true
 			return logDrain, nil
 		case 401:
@@ -102,34 +110,30 @@ func (c *Client) GetLogDrain(logDrainID int64) (*LogDrain, error) {
 		}
 	}
 
-	if response.Payload.ID == nil {
-		return logDrain, fmt.Errorf("payload.ID is a nil pointer")
-	}
-	logDrain.ID = *response.Payload.ID
-	logDrain.DrainType = *response.Payload.DrainType
-	logDrain.Handle = *response.Payload.Handle
-	logDrain.DrainType = *response.Payload.DrainType
-	logDrain.DrainHost = *response.Payload.DrainHost
-	logDrain.DrainPort = *response.Payload.DrainPort
-	logDrain.LoggingToken = *response.Payload.LoggingToken
-	logDrain.URL = *response.Payload.URL
-	logDrain.DrainApps = *response.Payload.DrainApps
-	logDrain.DrainDatabases = *response.Payload.DrainDatabases
-	logDrain.DrainEphemeralSessions = *response.Payload.DrainEphemeralSessions
-	logDrain.DrainProxies = *response.Payload.DrainProxies
+	logDrain.ID = swag.Int64Value(response.Payload.ID)
+	logDrain.DrainType = swag.StringValue(response.Payload.DrainType)
+	logDrain.Handle = swag.StringValue(response.Payload.Handle)
+	logDrain.DrainType = swag.StringValue(response.Payload.DrainType)
+	logDrain.DrainHost = swag.StringValue(response.Payload.DrainHost)
+	logDrain.DrainPort = swag.Int64Value(response.Payload.DrainPort)
+	logDrain.LoggingToken = swag.StringValue(response.Payload.LoggingToken)
+	logDrain.URL = swag.StringValue(response.Payload.URL)
+	logDrain.DrainApps = swag.BoolValue(response.Payload.DrainApps)
+	logDrain.DrainDatabases = swag.BoolValue(response.Payload.DrainDatabases)
+	logDrain.DrainEphemeralSessions = swag.BoolValue(response.Payload.DrainEphemeralSessions)
+	logDrain.DrainProxies = swag.BoolValue(response.Payload.DrainProxies)
 	return logDrain, nil
 }
 
-func (c *Client) DeleteLogDrain(logDrainID int64) error {
+func (c *Client) DeleteLogDrain(logDrainID int64) (bool, error) {
 	requestType := "deprovision"
 	request := models.AppRequest29{Type: &requestType}
 	params := operations.NewPostLogDrainsLogDrainIDOperationsParams().WithLogDrainID(logDrainID).WithAppRequest(&request)
 	op, err := c.Client.Operations.PostLogDrainsLogDrainIDOperations(params, c.Token)
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	operationID := *op.Payload.ID
-	_, err = c.WaitForOperation(operationID)
-	return err
+	return c.WaitForOperation(operationID)
 }
