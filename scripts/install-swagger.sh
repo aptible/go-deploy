9869be4d2df9f8aee5283c236a8e08fe7d39531e
Nat Welch <nat@natwelch.com>
2022-02-15 19:21:43 +0000

    Upgrade swagger version in script

--- scripts/install-swagger.sh
+++ scripts/install-swagger.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p bin
-download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/tags/v0.21.0 | jq -r '.assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
+download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/tags/v0.29.0 | jq -r '.assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
 curl -o bin/swagger -L'#' "$download_url"
 chmod +x bin/swagger
